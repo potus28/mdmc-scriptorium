48bed1b030fcaedd80152c451f8b9caf6eeb72ac
Woodrow N. Wilson <65731874+potus28@users.noreply.github.com>
2025-07-28 14:37:44 -0500

    Create run.py

---
+++ stitching_trajectories/ase/run-0/run.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from ase import Atoms, units
+from ase.calculators.lj import LennardJones
+
+from ase.md.langevin import Langevin
+from ase.md.velocitydistribution import (
+    Stationary,
+    ZeroRotation,
+    MaxwellBoltzmannDistribution,
+)
+
+
+
+
+x = np.linspace(0, 40, 4, endpoint= False)
+y = np.linspace(0, 40, 4, endpoint= False)
+z = np.linspace(0, 40, 4, endpoint= False)
+X,Y,Z= np.meshgrid(x,y,z)
+
+r = np.array([X.flatten(),Y.flatten(),Z.flatten()]).T
+
+atoms = Atoms("C64", r, cell = [40.0, 40.0, 40.0], pbc=True)
+
+
+
+calc = LennardJones(sigma = 3.73, epsilon = 148.0*units.kB, rc = 14.0, smooth = True)
+atoms.calc = calc
+
+dyn = Langevin(
+    atoms,
+    0.5 * units.fs,
+    temperature_K=150.0,
+    friction=0.01 / units.fs,
+    logfile="langevin.log",
+    trajectory="langevin.traj",
+    loginterval=8,
+)
+MaxwellBoltzmannDistribution(atoms, temperature_K=150)
+Stationary(atoms)
+ZeroRotation(atoms)
+
+
+dyn.run(2000)
+
